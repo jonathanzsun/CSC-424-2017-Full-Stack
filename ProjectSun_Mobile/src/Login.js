fdb8169a1de0c4340c165877dfabde8ecd34e01d
Michael Holec <Archsaint@gmail.com>
2017-02-22 20:31:37 -0600

    Create Login.js

---
+++ ProjectSun_Mobile/src/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Container from '../components/Container';
+import Button from '../components/Button';
+import Label from '../components/Label';
