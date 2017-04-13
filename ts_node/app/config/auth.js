bfac7425f1ff854161b00e8e95d0af3c610d3d63
Trevor Scheurer <trevor.scheurer@usm.edu>
2017-04-13 17:54:36 -0500

    removed all old ts_node stuff -TS

--- ts_node/app/config/auth.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Trevor on 2/18/2017.
- */
--- ts_node/app/config/database.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Trevor on 2/18/2017.
- */
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-//'url' : 'mongodb://127.0.0.1:27017/test'
-};
